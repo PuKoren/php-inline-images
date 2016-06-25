fa5f3ac2f2a8b9a1d06a2268c1365d8a16804762
Jonathan Muller <koren@pukogames.com>
2016-06-26 00:35:02 +0200

    Add SVG inliner

--- src/Converter.php
+++ src/Converter.php
@@ -11,7 +11,16 @@ class Converter {
         $fetcher = new Fetcher($this->path);
         $mime = $fetcher->getMimeType();
         if (strpos($mime, 'svg') !== false) {
-            return 'data:'.$mime.';'.trim($fetcher->getFileData(), " \t\n\r\0\x0B");
+            $content = explode(PHP_EOL, $fetcher->getFileData());
+            $output = array();
+
+            foreach($content as $line) {
+                if (strpos($line, '<?') === false && strpos($line, '<!') === false) {
+                    $output[] = str_replace('"', '\'', $line);
+                }
+            }
+
+            return 'data:'.$mime.';utf-8,'.implode('', $output);
         } else {
             //data:image/png;base64
             return 'data:'.$mime.';base64,'.base64_encode($fetcher->getFileData());
